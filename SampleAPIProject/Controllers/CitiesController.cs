c8a31454c3cc395031de7fd757512f09e1a69762
Gaurav Shahare <Gaurav@DESKTOP-IPQDE51>
2021-03-28 18:38:58 +0530

    second commit

---
+++ SampleAPIProject/Controllers/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleAPIProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPIProject.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+
+    }
+}
